3b2298070b9daec21c4cddfc518f0fc455d4cf66
Samahara <le.chat.au.chapeau@hotmail.com>
2017-12-04 15:32:54 -0600

    contador de caracteres y cambio de colores

--- js/app.js
+++ js/app.js
@@ -2,14 +2,42 @@
 var text = document.getElementById('message');
 var button = document.getElementById('twittear');
 var twittsArea = document.getElementById('twitts_container');
+var counter = document.getElementById('counter');
+
+//contador de caracteres
+var counterCharacter = function(){
+  // creando una variable count que inice en 0 y guarde la longitud de la cadena
+  // del text area.
+  var count = 0;
+  count = text.value.length;
+  counter.value = 140 - count;
+
+  if(count > 140){
+    button.disabled = true;
+    button.style.background = "#EEA5CC";
+    counter.style.color = "red";
+  }else if(count >= 130){
+    button.disabled = false;
+    button.style.background = "#D61E80";
+    counter.style.color = "orange";
+  }else if(count >= 120){
+    button.disabled = false;
+    button.style.background = "#D61E80";
+    counter.style.color = "green";
+  }else if(count <= 0){
+    button.disabled = true;
+    button.style.background= "#EEA5CC";
+  }else{
+    button.disabled = false;
+    button.style.background = "#D61E80";
+    counter.style.color = "gray";
+  }
+
+}
 
 /*
-  if(text === ""){
-    .addEventListener('', );
-  }else{
-    .addEventListener('', textUser);
-    button.style = #D61E80;
-  }
+var resizeTextArea = function(){
+
 }*/
 
 // crear una función que guarde el mensaje
@@ -24,7 +52,10 @@ var textUser = function(e) {
   messageContainer.appendChild(message);
   twittsArea.appendChild(messageContainer);
 
-  text.value = " ";
+  text.value = ""; //NOTA: NO DEJAR ESPACIOS EN BLANCO PORQUE LO RECONOCE COMO CARACTER
+  text.placeholder = "¿Qué está pasando?";
+
 }
 
+text.addEventListener("keyup", counterCharacter);
 button.addEventListener("click", textUser);
