f63afd7664fb0887f8d78c8d3e2ca2fd332cce84
Samahara <le.chat.au.chapeau@hotmail.com>
2017-12-05 17:02:59 -0600

    agregado button disabled

--- js/app.js
+++ js/app.js
@@ -4,6 +4,18 @@ var button = document.getElementById('twittear');
 var twittsArea = document.getElementById('twitts_container');
 var counter = document.getElementById('counter');
 
+// deshabilitar el botton si no hay texto
+var buttonOff = function(e) {
+  e.preventDefault();
+
+  if(text.value.length === 0){
+    button.disabled = true;
+    button.style.background = "#EEA5CC";
+  }else{
+    button.disabled = false;
+    button.style.background = "#D61E80";
+  }
+}
 // contador de caracteres
 var counterCharacter = function() {
   // creando una variable count que inice en 0 y guarde la longitud de la cadena
@@ -35,7 +47,7 @@ var counterCharacter = function() {
 
 }
 // agrandar la textarea conforme al texto
-var resizeTextArea = function(){
+var resizeTextArea = function() {
     text.style.height = "118px";
     text.style.height = text.scrollHeight + 18 + "px";
 }
@@ -63,6 +75,7 @@ var textUser = function(e) {
 }
 
 
+button.addEventListener("click", textUser);
+button.addEventListener("click", buttonOff);
 text.addEventListener("keyup", counterCharacter);
 text.addEventListener("keydown", resizeTextArea);
-button.addEventListener("click", textUser);
